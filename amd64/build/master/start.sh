ea9b7d19c76c24191378c2a404884e63c26fe87f
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2016-12-29 11:59:28 +0000

    A fix I believe, not like I tested or anything

--- amd64/build/master/start.sh
+++ amd64/build/master/start.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 if [ ! -f /var/lib/mfs/metadata.mfs ]; then
+  echo "copying empty metadata.mfs"
   cp /data/metadata.mfs /var/lib/mfs/metadata.mfs
 fi
 rm /var/lib/mfs/metadata.mfs.lock
 
+echo "chown mfs:mfs /var/lib/mfs/"
 chown mfs:mfs /var/lib/mfs/
+echo "chown mfs:mfs /var/lib/mfs/metadata.mfs"
+chown mfs:mfs /var/lib/mfs/metadata.mfs
+mfsmetarestore -a
 mfsmaster -d start
