82d43d671d4a6742f454d2c91fc1889ac03fed41
Fredrik Löwenhamn <fredrik.lowenhamn@gmail.com>
2016-12-29 13:09:46 +0000

    I'M PUSHING.

--- arm/build/master/start.sh
+++ arm/build/master/start.sh
@@ -3,8 +3,8 @@
 if [ ! -f /var/lib/lizardfs/metadata.mfs ]; then
   cp /data/metadata.mfs /var/lib/lizardfs/metadata.mfs
 fi
-rm /var/lib/lizardfs/metadata.mfs.lock
 
 chown lizardfs:lizardfs /var/lib/lizardfs/
 mfsmetarestore -a
+rm /var/lib/lizardfs/metadata.mfs.lock
 mfsmaster -d start
